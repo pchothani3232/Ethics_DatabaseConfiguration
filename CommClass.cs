65934875c922eeb2d52b94c399ba237c03b4afaa
PRIYA CHOTHANI <piyupatel3232@gmail.com>
2025-07-09 21:47:10 -0700

    Create CommClass.cs

---
+++ CommClass.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConfiguration
+{
+    public class CommClass
+    {
+        public string Connection { get; set; }
+    }
+}
